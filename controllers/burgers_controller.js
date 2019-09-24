1f5545d75bf04ce36c3d3d8fb0f7d0d6bd709cef
Stama Eleazar <eleoatam10@gmail.com>
2019-09-23 23:50:06 -0500

    fixed js fo dev btn

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,20 +6,20 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-// get route -> index
+
 router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
-  burger.all(function(burgInf) {
-    res.render("index", { burger_data: burgInf });
+  burger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
   });
 });
 
 router.post("/burgers/create", function(req, res) {
   burger.create(req.body.burger_name, function(result) {
-    //redirect to /burgers
+      //redirect to /burgers
     console.log(result);
     res.redirect("/");
   });
@@ -27,12 +27,9 @@ router.post("/burgers/create", function(req, res) {
 
 
 router.put("/burgers/:id", function(req, res) {
-  
-    burger.update(req.params.id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
+  burger.update(req.params.id, function(result) {
+   
     console.log(result);
-    // Send back response and let page reload from .then in Ajax
     res.sendStatus(200);
   });
 });
